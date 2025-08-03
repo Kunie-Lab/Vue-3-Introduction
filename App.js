5cfcd8defb707f9d9c0f7a99452e45bf6f33a333
kunie <kunie.dev@gmail.com>
2025-08-03 23:20:51 +0900

    Conditional Rendering

--- App.js
+++ App.js
@@ -2,10 +2,38 @@ import {defineComponent, ref} from "vue";
 
 export default defineComponent({
   setup() {
-    const message = ref('Hello vue!')
+    const activeTab = ref(-1);
+
     return {
-      message
+      activeTab,
     }
   },
-  template: `<div>{{ message }}</div>`
+  template: `
+    <div>
+      <form action="#" @reset="activeTab = -1">
+        <fieldset>
+          <legend>Tabs</legend>
+          <label for="activeTab_0">Tab 0</label>
+          <input type="radio" name="tab" :value="0" v-model="activeTab" id="activeTab_0"/>
+          <label for="activeTab_1">Tab 1</label>
+          <input type="radio" name="tab" :value="1" v-model="activeTab" id="activeTab_1"/>
+          <label for="activeTab_2">Tab 2</label>
+          <input type="radio" name="tab" :value="2" v-model="activeTab" id="activeTab_2"/>
+        </fieldset>
+        <button type="reset">Reset</button>
+      </form>
+      <div v-if="activeTab === 0" style="background-color: red">
+        Tab 0
+      </div>
+      <div v-else-if="activeTab === 1" style="background-color: green">
+        Tab 1
+      </div>
+      <div v-else-if="activeTab === 2" style="background-color: blue">
+        Tab 2
+      </div>
+      <div v-else>
+        Not 0/1/2
+      </div>
+    </div>
+  `
 });
