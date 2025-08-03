06a28538c2180f7787527c114e110babf6ac607c
kunie <kunie.dev@gmail.com>
2025-08-03 22:39:32 +0900

    Reactivity #1

--- App.js
+++ App.js
@@ -2,10 +2,21 @@ import {defineComponent, ref} from "vue";
 
 export default defineComponent({
   setup() {
-    const message = ref('Hello vue!')
+    const message = ref('Hello vue!');
+
+    const reverseMessage = () => {
+      message.value = message.value.split('').reverse().join('')
+    };
+
     return {
-      message
+      message,
+      reverseMessage,
     }
   },
-  template: `<div>{{ message }}</div>`
+  template: `
+    <div>
+      <p>{{message}}</p>
+      <button @click="reverseMessage">Reverse Message</button>
+    </div>
+  `
 });
